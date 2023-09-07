3409889ad12a1098152cfa4c988e1e583aa8306f
Tiwat <136233225+tiwazzz@users.noreply.github.com>
2023-09-07 20:23:41 +0700

    Change hour -> hours

--- function-style.py
+++ function-style.py
@@ -1,18 +1,21 @@
-def discount(hour, receipt):
+def discount(hours, receipt):
     if receipt > 0:
-        if hour <= 3:
+        if hours <= 3:
             discount = 0
-        elif hour <= 24:
-            discount = 20 + ((hour-3)*20)
+        elif hours <= 24:
+            discount = 20 + ((hours-3)*20)
         else:
-            hourMinus = hour-24
-            discount = 20 + (21*20) + (hourMinus*10)
+            hoursMinus = hours-24
+            discount = 20 + (21*20) + (hoursMinus*10)
     else:
-        if hour <= 3:
+        if hours <= 3:
             discount = 0
-        elif hour <= 24:
-            discount = 20 + ((hour-3)*20) - (receipt*0.05)
+        elif hours <= 24:
+            discount = 20 + ((hours-3)*20) - (receipt*0.05)
         else:
-            hourMinus = hour-24
-            discount = 20 + (21*20) + (hourMinus*10) - (receipt*0.05)
+            hoursMinus = hours-24
+            discount = 20 + (21*20) + (hoursMinus*10) - (receipt*0.05)
     return discount
+
+def result(hours, discount):
+    pass
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 numPlate = str(input("ป้อนเลขทะเบียนรถยนต์: "))
-hour = int(input("ชั่วโมงการจอดรถยนต์: "))
+hours = int(input("ชั่วโมงการจอดรถยนต์: "))
 buyChoice = str(input("ได้ซื้อของหรือไม่ (หากใช่ให้พิมพ์ yes หรือ y หากไม่ได้ซื้อให้พิมพ์ no หรือ n): "))
 
 # Configuration
@@ -9,27 +9,27 @@ receipt = 0
 
 if buyChoice == "yes" or buyChoice == "y":
     receipt = float(input("กรุณาป้อนยอดใบเสร็จของคุณ: "))
-    if hour <= 3:
+    if hours <= 3:
         discount = 0
         price = promotion
-    elif hour <= 24:
+    elif hours <= 24:
         discount = receipt*0.05
-        hourMinus = hour-3
-        price = promotion + (hourMinus*20) - discount
+        hoursMinus = hours-3
+        price = promotion + (hoursMinus*20) - discount
     else:
         discount = receipt*0.05
-        hourMinus = hour-24
-        price = (promotion + (21*20) + (hourMinus*10)) - discount
+        hoursMinus = hours-24
+        price = (promotion + (21*20) + (hoursMinus*10)) - discount
 
 elif buyChoice == "no" or buyChoice == "n":
-    if hour <= 3:
+    if hours <= 3:
         price = 20
-    elif hour <= 24:
-        hourMinus = hour-3
-        price = 20 + (hourMinus*20)
+    elif hours <= 24:
+        hoursMinus = hours-3
+        price = 20 + (hoursMinus*20)
     else:
-        hourMinus = hour-24
-        price = promotion + (21*20) + (hourMinus*10)
+        hoursMinus = hours-24
+        price = promotion + (21*20) + (hoursMinus*10)
 
 print(f"\n*********ใบเสร็จ*********")
 print(f"ทะเบียนรถยนต์: {numPlate}\nส่วนลดที่ได้รับ: {discount} บาท\nค่าใช้จ่ายที่ต้องชำระ: {price} บาท")
